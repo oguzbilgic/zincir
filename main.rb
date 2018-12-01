f652e0f4458524ce9767f66e7a9d29ba87ab9617
Oguz Bilgic <fisyonet@gmail.com>
2018-11-30 17:40:00 -0800

    Extract web.rb

--- main.rb
+++ main.rb
@@ -1,43 +1,13 @@
 require 'sinatra'
 require "sinatra/json"
 require 'httparty'
+
+require "./web.rb"
 require "./block.rb"
 require "./blockchain.rb"
 
 $blockchain = Blockchain.new
 $nodes = []
-PORT = 4000+rand(1000)
-
-class Web < Sinatra::Base
-  configure do
-    set :port, PORT
-    set :quiet, true
-    set :logging, false
-  end
-
-  post '/connect' do
-    puts "Node connected: #{params['ip']}"
-    $nodes << params['ip']
-  end
-
-  post '/relay' do
-    block = Block.from_json_str(request.body.read)
-    $blockchain.add_relayed_block block
-    puts "Received: #{block}"
-  end
-
-  get '/blocks' do
-    json $blockchain.last.to_hash
-  end
-
-  get '/blocks/:index' do
-    index = params['index'].to_i
-
-    return status 404 if index > $blockchain.last.index
-
-    json $blockchain.block_at(index).to_hash
-  end
-end
 
 # Web Thread
 Thread.new {
@@ -51,7 +21,7 @@ if ARGV[0]
   puts "Seed node: #{ARGV[0]}"
 
   # Connect to seed node
-  HTTParty.post "#{ARGV[0]}/connect", body: { ip: "http://localhost:#{PORT}" }
+  HTTParty.post "#{ARGV[0]}/connect", body: { ip: "http://localhost:#{$port}" }
 
   loop do
     index = $blockchain.last.index.to_i + 1
---
+++ web.rb
@@ -0,0 +1,33 @@
+# TODO Don't depend on global variables $blockchain, $nodes and $PORT
+$port = 4000+rand(1000)
+
+class Web < Sinatra::Base
+  configure do
+    set :port, $port
+    set :quiet, true
+    set :logging, false
+  end
+
+  post '/connect' do
+    puts "Node connected: #{params['ip']}"
+    $nodes << params['ip']
+  end
+
+  post '/relay' do
+    block = Block.from_json_str(request.body.read)
+    $blockchain.add_relayed_block block
+    puts "Received: #{block}"
+  end
+
+  get '/blocks' do
+    json $blockchain.last.to_hash
+  end
+
+  get '/blocks/:index' do
+    index = params['index'].to_i
+
+    return status 404 if index > $blockchain.last.index
+
+    json $blockchain.block_at(index).to_hash
+  end
+end
